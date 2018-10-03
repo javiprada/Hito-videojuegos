020e59c0da80c6440b92232ab369990b9ea94183
javiprada <javiprada0407@gmail.com>
2018-10-03 21:13:05 +0200

    primer commit

---
+++ Assets/CheckGround.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckGround : MonoBehaviour {
+
+    private Jugador_controlador player;
+
+	// Use this for initialization
+	void Start () {
+        player = GetComponentInParent<Jugador_controlador>();
+        
+		
+	}
+
+    void OnCollisionStay2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "Ground")
+        player.grounded = true;
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ground")
+            player.grounded = false;
+    }
+}
